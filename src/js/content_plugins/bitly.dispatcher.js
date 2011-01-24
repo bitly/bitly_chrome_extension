cdbfa2831981819055e0fb7c1bd7239e1173fad8
gregory80 <gregory.tomlinson@gmail.com>
2011-01-24 12:02:53 -0500

    updates

--- src/js/content_plugins/bitly.dispatcher.js
+++ src/js/content_plugins/bitly.dispatcher.js
@@ -1,8 +1,17 @@
 (function() {
     function dispatch_callback(jo) {}
     function init() {
-        var body, h = document.location && document.location.host;
-        if(h) {
+        var body, h = document.location && document.location.host, d_hosts=["bit.ly", "bitly.org", "j.mp", "bitly.net"];
+        if( d_hosts.indexOf( h ) >=0 ) {
+            var promos = document.getElementsByClassName("ext_bitly_chrome_promo_delay"), elem, mark=document.createElement("span");
+            for(var i=0; i<promos.length; i++) {
+                elem=promos[i];
+                elem.parentNode.removeChild(elem);
+            }
+            mark.className="has_bitly_chrome_ext_installed";
+            mark.setAttribute("style", "display:none;")
+            document.body.appendChild( mark  );
+        } else if(h) {
             chrome.extension.sendRequest({'action' : 'page_loaded', 'domain_host' : h || "" }, dispatch_callback);
         }
     }
